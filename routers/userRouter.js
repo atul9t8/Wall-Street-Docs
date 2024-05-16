cba9cccd024b3c76fdf47f95918aabfbb1784c24
atul9t8 <atuldebnath8@yahoo.com>
2024-05-16 16:32:40 +0600

    user signup method modified and login router added for user.

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { userSignup } = require("../controllers/userController");
+const { userSignup, userLogin } = require("../controllers/userController");
 
-router.get("/", userSignup);
+router.post("/", userSignup);
+router.post("/login", userLogin);
 
 module.exports = router;
