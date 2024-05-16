fe1795a16c4ac0276532a184b6cada520166377b
atul9t8 <atuldebnath8@yahoo.com>
2024-05-16 18:38:28 +0600

    separate function added for getting message from DB and count words API modified &  count character API added.

--- controllers/textCounterController.js
+++ controllers/textCounterController.js
@@ -1,11 +1,19 @@
 const Text = require("../models/textModel");
 
+const getTextFromDB = async (userId) => {
+  try {
+    const text = await Text.findOne({ user: userId });
+    return text ? text.text : null;
+  } catch (error) {
+    return res.status(500).send("Something went wrong! Please try again.");
+  }
+};
+
 const countWords = async (req, res) => {
   try {
-    let text = await Text.findOne({ user: req.user._id });
+    let text = await getTextFromDB(req.user._id);
     if (text !== null) {
-      let textFromDB = text.text;
-      const words = textFromDB.trim().split(/\s+/);
+      const words = text.trim().split(/\s+/);
       const wordCount = words.length;
       return res.status(200).send({ TotalWord: wordCount });
     } else {
@@ -16,4 +24,18 @@ const countWords = async (req, res) => {
   }
 };
 
-module.exports = { countWords };
+const countCharacters = async (req, res) => {
+  try {
+    let text = await getTextFromDB(req.user._id);
+    if (text !== null) {
+      const characterCount = text.length;
+      return res.status(200).send({ TotalCharacter: characterCount });
+    } else {
+      return res.status(404).send("No text available. Create one first.");
+    }
+  } catch {
+    return res.status(500).send("Something went wrong! Please try again.");
+  }
+};
+
+module.exports = { countWords, countCharacters };
