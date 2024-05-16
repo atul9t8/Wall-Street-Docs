0d4ebb45f56671475e19a545acdd509ab45d6ddd
atul9t8 <atuldebnath8@yahoo.com>
2024-05-16 18:38:55 +0600

    count characters routre added to text count router.

--- routers/textCounterRouter.js
+++ routers/textCounterRouter.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { countWords } = require("../controllers/textCounterController");
+const {
+  countWords,
+  countCharacters,
+} = require("../controllers/textCounterController");
 
 const authorize = require("../middlewares/authorize");
 
 router.get("/words", [authorize], countWords);
+router.get("/character", [authorize], countCharacters);
 
 module.exports = router;
