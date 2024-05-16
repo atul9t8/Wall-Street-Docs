1124dbe7f9042f5b08cb97c23cdc64b0cd5d787a
atul9t8 <atuldebnath8@yahoo.com>
2024-05-16 18:48:38 +0600

    count sentence route  added to text count router.

--- routers/textCounterRouter.js
+++ routers/textCounterRouter.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const {
   countWords,
   countCharacters,
+  countSentence,
 } = require("../controllers/textCounterController");
 
 const authorize = require("../middlewares/authorize");
 
 router.get("/words", [authorize], countWords);
 router.get("/character", [authorize], countCharacters);
+router.get("/sentence", [authorize], countSentence);
 
 module.exports = router;
