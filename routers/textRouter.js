c4347dd53aea0a64ec77c1f2532b85aacfe44810
atul9t8 <atuldebnath8@yahoo.com>
2024-05-17 17:17:29 +0600

    delete text route added to text router.

--- routers/textRouter.js
+++ routers/textRouter.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { createOrUpdateText } = require("../controllers/textController");
+const {
+  createOrUpdateText,
+  deleteText,
+} = require("../controllers/textController");
 
 const authorize = require("../middlewares/authorize");
-const user = require("../middlewares/user");
 
 router.post("/", [authorize], createOrUpdateText);
+router.delete("/delete", [authorize], deleteText);
 
 module.exports = router;
